7e9b7ba95bca00cb5a5f7ec9cd4fb26147f327ff
valdinei-ads <valdinei.ads@gmail.com>
2021-03-29 01:43:30 -0300

    Feature/consultas de contas (#4)
    * Criação de contas.
    
    * Consulta de Contas

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { promises as fs } from 'fs';
 import path from 'path';
+
 const {readFile, writeFile} = fs;
 
 const routes = express.Router();
@@ -9,7 +10,9 @@ routes.post('/', async (request, response) => {
     try {
         let account = request.body;
 
-        const pathFile = path.resolve('accounts.json' );
+        const filePath = path.resolve('accounts.json' );
+
+        const data = JSON.parse(await readFile(filePath));
 
         const data = JSON.parse(await readFile(pathFile));
         
@@ -17,9 +20,41 @@ routes.post('/', async (request, response) => {
 
         data.accounts.push(account);
 
-        await writeFile(pathFile, JSON.stringify(data, null, 2));
+        await writeFile(filePath, JSON.stringify(data, null, 2));
         
         response.send(account);
+    } catch (err) {
+        response.status(400).send({ error: err.message})        
+    }
+});
+
+routes.get('/', async (request, response) => {
+    try {
+        
+        const filePath = path.resolve('accounts.json' );
+        
+        const data = JSON.parse(await readFile(filePath));
+
+        response.send(data);
+        
+    } catch (err) {
+        response.status(400).send({ error: err.message})        
+    }
+});
+
+routes.get('/:id', async (request, response) => {
+    try {
+
+        let id = request.params.id;
+        
+        const filePath = path.resolve('accounts.json' );
+        
+        const data = JSON.parse(await readFile(filePath));
+        
+        let filtered = data.accounts.filter(x => x.id == id);
+        
+        response.send(filtered);
+        
     } catch (err) {
         response.status(400).send({ error: err.message})        
     }

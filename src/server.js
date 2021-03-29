3a0fe5181ee1fe4c55f9c499a78b5adf170c6084
valdinei-ads <valdinei.ads@gmail.com>
2021-03-29 00:08:13 -0300

    Commit da base da aplicação. (#1)

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express, { json } from 'express';
+import routes from './routes/route.js';
+import {promises as fs} from 'fs';
+
+const {readFile, writeFile} = fs
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, async () => {
+    try {
+        await readFile('accounts.json').then(() => {
+            console.log('API Started!!!');
+        });
+    } catch (error) {
+        
+        const initialJson = { nextId: 1, accounts: [] }
+
+        await writeFile('accounts.json', JSON.stringify(initialJson)).then(() => {
+            console.log('API started and File Created!!!');
+        }).catch(error => {
+            console.log(error);
+        });
+
+    }
+});

5a08bb54c6e96eabd6b3f05a1f933deee55e35c4
DESKTOP-JPM3F7G\Valdinei Almeida <valdinei.ads@gmail.com>
2021-03-29 01:25:04 -0300

    Criação de contas.

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import accountController from '../controllers/accountController.js'
 
 const routes = express.Router();
+
+routes.use('/accounts', accountController);
 
 routes.get('/', (request, response) => {
     return response.json({message: 'Hello World!!'});
